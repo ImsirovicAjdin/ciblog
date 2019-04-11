7afde51304481b477981e2d12b2ca84528741a60
onlinebiznisinfo <46485361+onlinebiznisinfo@users.noreply.github.com>
2019-04-11 20:41:12 +0200

    Configure base url, add it to navbar links, set up routes for localhost/ciblog/about, update autoload url helpers, echo out title in the about page, and add navbar to site header.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,6 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'pages/view';
-$rout['(:any)'] = 'pages/view/$1';
+$route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
