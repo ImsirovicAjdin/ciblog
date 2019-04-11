d46e514de214d2529dd4ed22bca3bbaade89c890
onlinebiznisinfo <46485361+onlinebiznisinfo@users.noreply.github.com>
2019-04-11 20:49:16 +0200

    Fix navbar brand linking to proper project location, fix tag linting as required by IntelliJ

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
 			rel="stylesheet"
 			href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/flatly/bootstrap.min.css">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="/">ciBlog</a>
+			<a class="navbar-brand" href="<?= base_url(); ?>">ciBlog</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse"
 					data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
