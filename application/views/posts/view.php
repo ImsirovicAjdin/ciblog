f8ea256238d7819f553e79e5d876ffb7aff224cb
FlowOverstack <ajdika@live.com>
2019-04-12 22:05:56 +0200

    Add base_url() to posts/edit on Edit button in views/posts/view.php

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -12,5 +12,5 @@
  c/a -->
 	<input type="submit" value="Delete" class="btn btn-danger d-inline">
 </form>
-<a class="btn btn-warning" href="edit/<?php echo $post['slug']; ?>">Edit</a>
+<a class="btn btn-warning" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
 
