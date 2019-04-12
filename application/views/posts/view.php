0455d44ee8235c20ac219328e93df8395729e321
FlowOverstack <ajdika@live.com>
2019-04-12 20:26:42 +0200

    Fix the form_open link to be able to delete a post

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -11,7 +11,7 @@
 </div>
 
 <hr>
-<?php echo form_open('/posts/delete'.$post['id']); ?> <!-- this post will submit to /posts/delete/
+<?php echo form_open('/posts/delete/'.$post['id']); ?> <!-- this post will submit to /posts/delete/
  c/a -->
 	<input type="submit" value="delete" class="btn btn-danger">
 </form>
