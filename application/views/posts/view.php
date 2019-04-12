47291583209242ad81919e949ad3a81753fef75a
FlowOverstack <ajdika@live.com>
2019-04-12 22:02:50 +0200

    Update view by removing redundant comments

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,9 +1,3 @@
-<!--
-<pre>
-<?php //print_r($post);die; ?>
-<?php //var_dump($post);die; ?>
-</pre>
--->
 <h2><?php echo $post['title']; ?></h2>
 
 <small class="badge badge-primary d-inline-block">Posted on: <?php echo $post['created_at'];
