51d3c49cc932cc078e0faaa96a5a718a886ba322
FlowOverstack <ajdika@live.com>
2019-04-13 07:49:09 +0200

    Show uploaded image in the post when viewing it

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,11 +1,26 @@
 <h2><?= $title ?></h2>
 <?php foreach($posts as $post) : ?>
 	<h3><?php echo $post['title']; ?></h3>
-	<small class="badge badge-primary d-inline-block">
-		Posted on: <?php echo $post['created_at']; ?>
-		in <strong><?php echo $post['name']; ?></strong>
-	</small><br>
-	<?php echo word_limiter($post['body'], 60); ?>
-	<p><a class="btn btn-success" href="<?= site_url('/posts/'.$post['slug']) ; ?>">Read
-			More</a></p>
+
+	<div class="row">
+		<div class="col-md-3">
+			<!-- FIRST VERSION, LARGE IMAGES BREAK THE LAYOUT
+			<img src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image']; ?>">
+			-->
+			<!-- SECOND VERSION, LARGE IMAGES ARE CONTAINED WITH THE BOOTSTRAP IMAGE CLASS -->
+			<img class="img-fluid" src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['post_image'];
+			?>">
+		</div>
+		<div class="col-md-9">
+			<small class="badge badge-primary d-inline-block">
+				Posted on: <?php echo $post['created_at']; ?>
+				in <strong><?php echo $post['name']; ?></strong>
+			</small><br>
+			<?php echo word_limiter($post['body'], 60); ?>
+			<p><a class="btn btn-success" href="<?= site_url('/posts/'.$post['slug']) ; ?>">Read
+					More</a></p>
+		</div>
+	</div>
+
+
 <?php endforeach; ?>
