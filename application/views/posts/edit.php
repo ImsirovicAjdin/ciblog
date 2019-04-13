cd96617e1f01225aec3f4fdce31db8447a474475
FlowOverstack <ajdika@live.com>
2019-04-13 06:47:33 +0200

    Make categories available in view/posts/edit view

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -3,9 +3,7 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('posts/update'); ?>
-
-		<input type="hidden" name="id" value="<?php echo $post['id']; ?>">
-
+	<input type="hidden" name="id" value="<?php echo $post['id']; ?>">
 	<div class="form-group">
 		<label>Title</label>
 		<input name="title" type="text" class="form-control" placeholder="<?php echo $post['title']; ?>"
@@ -16,6 +14,14 @@
 		<textarea name="body" class="form-control" placeholder="<?php echo $post['body']; ?>">
 			<?php echo $post['body']; ?>
 		</textarea>
+	</div>
+	<div class="form-group">
+		<label>Category</label>
+		<select name="category_id" class="form-control">
+			<?php foreach($categories as $category): ?>
+				<option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+			<?php endforeach; ?>
+		</select>
 	</div>
 	<button type="submit" class="btn btn-default">Submit</button>
 </form>
