d1c8f3c4472f23bd9e6e51e702372547108296b0
FlowOverstack <ajdika@live.com>
2019-04-13 10:10:09 +0200

    Setup (:any) ... $1 routes for specific category link in $routes; remove old comments to make space for new ones

--- application/config/routes.php
+++ application/config/routes.php
@@ -11,6 +11,10 @@ $route['default_controller'] = 'pages/view';
 
 $route['categories'] = 'categories/index';
 $route['categories/create'] = 'categories/create';
+// when click a category link on ciblog/categories, it takes you to e.g categories/posts/2
+// i.e it takes you to categories/posts/$id (where $id is the `post`.`id`
+// here's the fix for this issue:
+$route['categories/posts/(:any)'] = 'categories/posts/$1'; // $1 is basically a placeholder
 
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -1,30 +1,26 @@
 <?php
 
 class Categories extends CI_Controller {
-	// (2)
 	public function index() {
 		$data['title'] = 'Categories';
 
 		$data['categories'] = $this->category_model->get_categories();
 
 		$this->load->view('templates/header');
-		$this->load->view('categories/index', $data); // we wanna load categories/index (V) and also pass
-		// along our $data array
+		$this->load->view('categories/index', $data);
 		$this->load->view('templates/footer');
 	}
 
 
-	// (1)
 	public function create() {
 		$data['title'] = 'Create Category';
 
-		// when we submit our form, we want to validate our category name
 		$this->form_validation->set_rules('name', 'Name', 'required');
 
 		// now we need to run it through a validator
 		if($this->form_validation->run() === FALSE) {
 			$this->load->view('templates/header');
-			$this->load->view('categories/create', $data); // and we also wanna pass along our $data array
+			$this->load->view('categories/create', $data);
 			// to the view (which includes the ['title']
 			$this->load->view('templates/footer');
 		} else {
