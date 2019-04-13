7d3c4b8142f7e47032971c5675eeadda4f52738f
FlowOverstack <ajdika@live.com>
2019-04-13 06:40:09 +0200

    Remove comments that describe how we're joining tables in get_posts inside the Posts model; add the category name inside the posts/view/index because we have now made it available to our V inside the Posts M

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,8 +1,10 @@
 <h2><?= $title ?></h2>
 <?php foreach($posts as $post) : ?>
 	<h3><?php echo $post['title']; ?></h3>
-	<small class="badge badge-primary d-inline-block">Posted on: <?php echo $post['created_at'];
-	?></small><br>
+	<small class="badge badge-primary d-inline-block">
+		Posted on: <?php echo $post['created_at']; ?>
+		in <strong><?php echo $post['name']; ?></strong>
+	</small><br>
 	<?php echo word_limiter($post['body'], 60); ?>
 	<p><a class="btn btn-success" href="<?= site_url('/posts/'.$post['slug']) ; ?>">Read
 			More</a></p>
