3f94e3cf966f50857558fc422af77c9b1785d88b
FlowOverstack <ajdika@live.com>
2019-04-14 09:48:07 +0200

    Alter categories table with user_id so that we can allow only the author of a category to delete it; add if condition to views/categories/index.php so that it checks the userdata['user_id'] against $category['user_id']; oh, and also, add __notes

--- application/views/categories/index.php
+++ application/views/categories/index.php
@@ -5,9 +5,22 @@
 		<a href="<?php echo site_url('/categories/posts/'.$category['id']);	?>">
 			<?php echo $category['name']; ?>
 		</a>
+		<?php if($this->session->userdata('user_id') == $category['user_id']): ?>
+			DELETE
+		<?php endif; ?>
 	</li>
 <?php endforeach; ?>
 </ul>
+
+<!--
+We want to see if the current category item user ID matches the logged in user now we did this in our post,
+ if we go to the post view and view dot php right here, this is the logic that we need:
+
+ <?php // if($this->session->userdata('user_id')) == $post['user_id']): ?>
+
+ We just wanna change this to category ID user ID. So let's test it out, let's just say delete if
+ the user has the correct ID.
+-->
 
 
 
